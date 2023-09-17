c17423f44678182ad387b99342d4f5cb8eae8e23
Scratch <Scrxtchy@users.noreply.github.com>
2023-09-17 21:22:22 +1000

    QR codes from OTPListener

--- src/XIVLauncher.Common/Http/OtpListener.cs
+++ src/XIVLauncher.Common/Http/OtpListener.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using QRCoder;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Threading;
+using System.Collections.Generic;
 
 namespace XIVLauncher.Common.Http
 {
@@ -15,12 +20,36 @@ namespace XIVLauncher.Common.Http
 
         private readonly Thread serverThread;
 
+        public readonly (byte[] qr, string ip)[] qrcodes;
+
+        private QRCodeGenerator qrGenerator = new();
+
         public OtpListener(string version)
         {
             this.server = new HttpServer(HTTP_PORT, version);
             this.server.GetReceived += this.GetReceived;
 
             this.serverThread = new Thread(this.server.Start) { Name = "OtpListenerServerThread", IsBackground = true };
+
+            this.qrcodes = PopulateQrList();
+        }
+
+        private (byte[] qr, string ip)[] PopulateQrList()
+        {
+            List<(byte[] qr, string ip)> qrcodes = new();
+            qrGenerator.CreateQrCode("test", QRCodeGenerator.ECCLevel.L);
+            NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface adapter in interfaces)
+            {
+                if (adapter.OperationalStatus == OperationalStatus.Up)
+                {
+                    foreach (var ip in adapter.GetIPProperties().UnicastAddresses.Where(ip => ip.Address.AddressFamily == AddressFamily.InterNetwork))
+                    {
+                        qrcodes.Add((new BitmapByteQRCode(qrGenerator.CreateQrCode($"http://{ip.Address}:{HTTP_PORT}/ffxivlauncher/", QRCodeGenerator.ECCLevel.L)).GetGraphic(1), ip.Address.ToString()));
+                    }
+                }
+            }
+            return qrcodes.ToArray();
         }
 
         private void GetReceived(object sender, HttpServer.HttpServerGetEvent e)
